969265a7d1d3cea3b87817aa1821fb5096e4c5b3
oggyyy2000 <tuanbeos47@gmail.com>
2023-05-09 14:43:40 +0700

    first commit

---
+++ src/component/photoms/itemData.js
@@ -0,0 +1,9 @@
+const itemData = [
+  {
+    img: "http://epsmarttech.com.vn:8000/dotkiemtra_import_db/T87_2023-01-04/VT2_T87-VT16_T87/dkt_details/07h41p/e489e32e-d015-4b2b-b8f6-8fc79ae21884.jpg",
+    title: "Cột Điện",
+    type: "5",
+  },
+];
+
+export default itemData;
